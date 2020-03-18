8b8531c6c4943a4d776a59683e118db0fb5e27cf
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-03-19 00:18:29 +0100

    adding some style to the blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import {Link, graphql, useStaticQuery} from "gatsby"
+import blogStyles from "./blog.module.scss"
 
 const BlogPage = () => {
 
@@ -20,16 +21,17 @@ const BlogPage = () => {
    return(
     <Layout>
     <h1>Blog</h1>
-    <ol>
+    <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((edge, i)=> {
             return(
-                <li key={i}>
+                <li key={i} className={blogStyles.post}>
                 <Link to={`/blog/${edge.node.fields.slug}`}>
                     <h2>{edge.node.frontmatter.title} </h2>
-                    </Link>
+                    
                     <p>{edge.node.frontmatter.date}</p>
-               
+                </Link>
                 </li>
+                
             )})
         }
     </ol>
