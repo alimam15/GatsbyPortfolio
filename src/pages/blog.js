fdb2f8d0809dc2e169a6f4e26de4ecdf261fc834
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-03-18 18:14:42 +0100

    adding links to post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import {graphql, useStaticQuery} from "gatsby"
+import {Link, graphql, useStaticQuery} from "gatsby"
 
 
 
@@ -14,7 +14,9 @@ const BlogPage = () => {
                         frontmatter{
                         title
                         date
-                        }  
+                        }  fields{
+                            slug
+                        }
                     }
                     }
                 }
@@ -29,8 +31,11 @@ const BlogPage = () => {
         {data.allMarkdownRemark.edges.map((edge)=> {
             return(
                 <li>
+                <Link to={`/blog/${edge.node.fields.slug}`}>
                     <h2>{edge.node.frontmatter.title} </h2>
+                    </Link>
                     <p>{edge.node.frontmatter.date}</p>
+               
                 </li>
             )})
         }
