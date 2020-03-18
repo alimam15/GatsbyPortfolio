d46566bc839314909d795901207e1b3080502ac0
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-03-18 15:48:29 +0100

    fetching posts date, title from md, and printing on blog post

--- src/components/header.js
+++ src/components/header.js
@@ -20,15 +20,19 @@ const Header = () =>{
             <ul className={headerStyles.navList}>
                 <li>
                     <Link className = {headerStyles.navItem} 
-                activeClassName={headerStyles.activeNavItem} to="/">home</Link>
+                activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
                 </li>
                 <li>
                     <Link className = {headerStyles.navItem} 
-                activeClassName={headerStyles.activeNavItem}to="/about">about</Link>
+                activeClassName={headerStyles.activeNavItem}to="/about">About</Link>
                 </li>
                 <li>
                     <Link className = {headerStyles.navItem} 
-                activeClassName={headerStyles.activeNavItem}to="/contact">contact</Link>
+                activeClassName={headerStyles.activeNavItem}to="/blog">Blog</Link>
+                </li>
+                <li>
+                    <Link className = {headerStyles.navItem} 
+                activeClassName={headerStyles.activeNavItem}to="/contact">Contact</Link>
                 </li>
             </ul>
         </nav>
---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const page404 = ()=>(
+    <Layout>
+        <h1>Page 404</h1>
+        <p>page not found!</p>
+    </Layout>
+)
+
+export default page404
