cca0ec52d04cdf0b545bdb0bdb72aa2dc03871c5
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-03-19 05:20:30 +0100

    Finally connecting cms contentfull api

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
   },
 
   plugins : [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+          spaceId: process.env.CONTENTFUL_SPACE_ID,
+          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+  },
     'gatsby-plugin-sass',
     {
       resolve: `gatsby-source-filesystem`,
