5c82f170b3b16ecb8d59b3b092bb0325e77d03ac
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-03-21 00:44:56 +0100

    adding dynamic contentful pages with pics

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout  from "../components/layout"
 import {graphql} from "gatsby"
+import {documentToReactComponent, documentToReactComponents} from "@contentful/rich-text-react-renderer"
 
 // Old Query for Markdown purpose
 /*export const query = graphql`query(
@@ -19,15 +20,30 @@ export const query = graphql`query($slug : String!){
   contentfulBlogPost(slug:{eq: $slug}){
     title
     publishedDate(formatString:"MMMM Do, YYYY")
+    body{
+      json
+    }
 
   }
 }`
 
 const Blog = (props) => {
+  const {title, publishedDate} = props.data.contentfulBlogPost
+  const options ={
+    renderNode: {
+    "embedded-asset-block": (node) =>{
+      const alt = node.data.target .fields.title['en-US']
+      const url= node.data.target .fields.file['en-US'].url
+      return <img alt = {alt} src = {url}/>
+    }
+    }
+
+  }
    return(
      <Layout>
-     <h1>{props.data.contentfulBlogPost.title}</h1>
-     <p>{props.data.contentfulBlogPost.publishedDate}</p>
+     <h1>{title}</h1>
+     <p>{publishedDate}</p>
+     {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
      </Layout>
    )
    }
