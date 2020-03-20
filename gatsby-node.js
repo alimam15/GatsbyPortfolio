5ae7f86f24aeebdf19fae3180c2851b0c544124f
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-03-20 23:57:16 +0100

    adding dynamic pages contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,39 @@
 const path = require('path')
 
-module.exports.onCreateNode = ({node, actions}) => {
+/* create node for markdown purpose*/
+/* module.exports.onCreateNode = ({node, actions}) => {
     const {createNodeField} = actions 
     if(node.internal.type === 'MarkdownRemark'){
         const slug = path.basename(node.fileAbsolutePath, '.md')
         /*console.log(slug)
         console.log(JSON.stringify(node, undefined, 4))*/
-        createNodeField({
+       /* createNodeField({
             node,
             name: 'slug',
             value:slug
         })}
-    }
+    }*/
+    
 module.exports.createPages = async ({graphql, actions}) => {
     const { createPage} = actions
     const blogTemplate = path.resolve(`./src/templates/blog.js`)
     const res = await graphql(`query{
-                                    allMarkdownRemark{
+                                    allContentfulBlogPost{
                                         edges{
                                             node{
-                                                fields{
-                                                    slug
-                                                }
+                                                slug
                                             }
                                         }
                                     }
                                 }`)
     
-    res.data.allMarkdownRemark.edges.forEach((edge) => {
+     /* For markdown, be careful where the slug lives under edge.node.fields.slug */
+    res.data.allContentfulBlogPost.edges.forEach((edge) => {
         createPage({
             component:blogTemplate,
-            path: `/blog/${edge.node.fields.slug}`,
+            path: `/blog/${edge.node.slug}`,
             context: {
-                slug: edge.node.fields.slug
+                slug: edge.node.slug
             }
         })
         
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,29 +2,37 @@ import React from 'react'
 import Layout  from "../components/layout"
 import {graphql} from "gatsby"
 
+// Old Query for Markdown purpose
+/*export const query = graphql`query(
+  $slug : String!){markdownRemark(fields:{slug:{eq: $slug }})
+            {frontmatter{titledate}html}}`*/
+//Old formatting
+  //const {title, date} = props.data.markdownRemark.frontmatter
+  /*return(
+      <Layout>
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={{__html:props.data.markdownRemark.html}}></div>
+      </Layout>)*/
 
 export const query = graphql`query($slug : String!){
-    markdownRemark(
-      fields:{
-        slug:{
-          eq: $slug
-        }})
-            {frontmatter{
-                      title
-                    date}
-            html}}`
-const Blog = (props) => {
-  
-   const {title, date} = props.data.markdownRemark.frontmatter
+  contentfulBlogPost(slug:{eq: $slug}){
+    title
+    publishedDate(formatString:"MMMM Do, YYYY")
 
-    return(
-        <Layout>
-        <h1>{title}</h1>
-        <p>{date}</p>
-        <div dangerouslySetInnerHTML={{__html:props.data.markdownRemark.html}}></div>
-        </Layout>
-        
-    )
-}
+  }
+}`
+
+const Blog = (props) => {
+   return(
+     <Layout>
+     <h1>{props.data.contentfulBlogPost.title}</h1>
+     <p>{props.data.contentfulBlogPost.publishedDate}</p>
+     </Layout>
+   )
+   }
+
+ 
+
 
 export default Blog
