b628af902fb2689f2486e0770119d24b7cf9d4be
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-03-20 23:28:05 +0100

    rendering contentful Posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,31 +4,33 @@ import {Link, graphql, useStaticQuery} from "gatsby"
 import blogStyles from "./blog.module.scss"
 
 const BlogPage = () => {
-
-    const data = useStaticQuery(graphql`
-        query{
-                allMarkdownRemark{
-                    edges{
-                    node{
-                        frontmatter{
-                        title
-                        date
-                        }  fields{
-                            slug
-                        }}}} }
-                                `)
+    /* Old query using markdonw Remark */
+    /*const data = useStaticQuery(graphql`query{allMarkdownRemark{edges{ node{frontmatter{title
+            date}  fields{slug }}}} } `)*/
+    const data = useStaticQuery(graphql`query{
+        allContentfulBlogPost(sort:{fields: publishedDate, order: DESC}){
+            edges{
+                node{
+                    title
+                    slug
+                    publishedDate(formatString: "MMMM Do, YYYY")
+                }
+            }
+        }
+     } 
+                `)
                               
    return(
     <Layout>
     <h1>Blog</h1>
     <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map((edge, i)=> {
+        {data.allContentfulBlogPost.edges.map((edge, i)=> {
             return(
                 <li key={i} className={blogStyles.post}>
-                <Link to={`/blog/${edge.node.fields.slug}`}>
-                    <h2>{edge.node.frontmatter.title} </h2>
+                <Link to={`/blog/${edge.node.slug}`}>
+                    <h2>{edge.node.title} </h2>
                     
-                    <p>{edge.node.frontmatter.date}</p>
+                    <p>{edge.node.publishedDate}</p>
                 </Link>
                 </li>
                 
