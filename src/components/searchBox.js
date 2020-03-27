cf9b666c34a1bf8fe0be2a63b8b4a5f3e0be33d3
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-03-27 23:28:57 +0100

    sytling search

--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -4,7 +4,10 @@ import searchBoxStyles from "./searchBox.module.scss"
 const SearchBox = ({searchfield ,searchChange}) => {
     return(
                 <div >
-                <input type = "search" placeholder="Search for a blog" onChange={searchChange}></input>
+                <input type = "search"
+                    placeholder="Search for a blog" 
+                    onChange={searchChange}
+                    className={searchBoxStyles.search}></input>
                 </div>       
     )
 }
