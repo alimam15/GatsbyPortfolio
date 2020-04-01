43cbb84465f0ba6644a55a63d1007ffb3ed5956f
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-04-01 19:42:56 +0100

    NS 5

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,15 +3,15 @@ import Layout from "../components/layout"
 import Head from "../components/head"
 
 
-const AboutPage = () => (
-
+const AboutPage = () => {
+return(
     <Layout>
         <Head title="about" />
         <h1>About Page</h1>
         <h3>This is the about page</h3>
         <p>Here you can add some content about your website.</p>
     </Layout>
-
-)
+    )
+}
 
 export default AboutPage
