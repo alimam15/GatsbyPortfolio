affcd0b9257b5385a468844fe822b6359071645b
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-04-02 23:22:52 +0100

    NS 9

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
 
-
 const ContactPage = () => (
     <Layout>
         <Head title="contact" />
